f3a69a79dd5dc88fe3f48e9c4645432ab89f9e92
Felix Krause <flyx@isobeef.org>
2013-07-09 17:12:18 +0200

    Made it possible to bundle GPS built with GNAT FSF

--- bundle/build-bundle.sh
+++ bundle/build-bundle.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
+GNAT_KIND=${GNAT_KIND:-GPL} 
+if [ "$GNAT_KIND" != "GPL" ] && [ "$GNAT_KIND" != "FSF" ] ; then
+   echo "Unsupported GNAT_KIND: $1";
+   exit(-1);
+fi
+if [ "$GNAT_KIND" != "FSF" ] ; then
+   export GNAT_HOME=/usr/local/gnat
+else
+   export GNAT_HOME=/opt/gcc-4.8.1
+fi
+
 export PATH=$PREFIX/bin:$PATH
 rm -rf GPS.app
 gtk-mac-bundler gps.bundle
