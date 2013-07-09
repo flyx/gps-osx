71eadcfb90b15a8c3bc3af71bb4b2d13e510e706
Felix Krause <flyx@isobeef.org>
2013-07-09 16:58:48 +0200

    Added possibility to compile with GNAT FSF

--- build-gps.sh
+++ build-gps.sh
@@ -1,3 +1,10 @@
+GNAT_KIND=${GNAT_KIND:-GPL} 
+
+if [ "$GNAT_KIND" != "GPL" ] && [ "$GNAT_KIND" != "FSF" ] ; then
+   echo "Unsupported GNAT_KIND: $1";
+   exit(-1);
+fi
+
 export GPR_PROJECT_PATH=$PREFIX/lib/gnat
 export PATH=$PREFIX/bin:$PATH
 # gnat gcc does not support -arch
@@ -7,6 +14,10 @@ cd gps-release-ide-5.2.1-src
 ./configure --prefix=$PREFIX --with-python=$PREFIX/bin/python --enable-gpl
 sed  '/Python_Libs/c \
    Python_Libs    := ("-ldl", "-Wl,-framework", "-Wl,CoreFoundation", "-lpython2.7");' gnatlib/gnatcoll_shared.gpr > gnatlib/gnatcoll_shared.gpr.patched
+if [ "$GNAT_KIND" == "FSF" ] ; then
+   # FSF GNAT does not know -fdump-xref
+   sed -i 's/, "-fdump-xref"//' gnatlib/gnatcoll_shared.gpr.patched
+fi
 cp gnatlib/gnatcoll_shared.gpr.patched gnatlib/gnatcoll_shared.gpr
 # because git cannot manage folders
 mkdir -p gnatlib/obj gnatlib/lib spark/obj spark/lib gnat obj lib
